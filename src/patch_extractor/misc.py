3a58fee3f36064091b8e6085ae71e4038ff981bf
Jim Denholm <kxhf345@astrazeneca.net>
2025-01-01 15:07:38 +0000

    :sparkles: Added functions for checking dtypes and shape

--- src/patch_extractor/misc.py
+++ src/patch_extractor/misc.py
@@ -1,6 +1,23 @@
 """Miscellaneous functions."""
 
-from numpy import ndarray
+from numpy import ndarray, uint8
+
+
+def is_rgb_uint8(img: ndarray) -> bool:
+    """Return ``True`` if ``img`` is RGB and uint8, else ``False``.
+
+    Parameters
+    ----------
+    img : ndarray
+        An image whose RGB uint8 status is to be checked.
+
+    Returns
+    -------
+    bool
+        Whether the image is RGB and uint8.
+
+    """
+    return is_rgb(img) and is_uint8(img)
 
 
 def is_rgb(img: ndarray) -> bool:
@@ -14,6 +31,24 @@ def is_rgb(img: ndarray) -> bool:
     Returns
     -------
     bool
+        Whether the image is RGB or not.
 
     """
     return (img.ndim == 3) and (img.shape[-1] == 3)
+
+
+def is_uint8(img: ndarray) -> bool:
+    """Return ``True`` if ``img`` has uint8 dtype, else ``False``.
+
+    Parameters
+    ----------
+    img : ndarray
+        An image whose uint8 status is to be checked.
+
+    Returns
+    -------
+    bool
+        If the image is of dtype uint8.
+
+    """
+    return img.dtype == uint8
